6af82886329fb2f580a99dc366863bcdd382db1e
김강민 <wassw@naver.com>
2020-05-22 14:25:32 +0900

    [App] BottomNavigationView
    BottomNavigationView
    Main/Recipe/Map/Mypage 생성
     
     S02P31D102-68

---
+++ app/app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.myapplication.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.myapplication.R;
+import com.example.myapplication.fragment.FragmentMain;
+import com.example.myapplication.fragment.FragmentMap;
+import com.example.myapplication.fragment.FragmentMypage;
+import com.example.myapplication.fragment.FragmentRecipe;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentMain fragmentMain = new FragmentMain();
+    private FragmentRecipe fragmentRecipe = new FragmentRecipe();
+    private FragmentMap fragmentMap = new FragmentMap();
+    private FragmentMypage fragmentMypage = new FragmentMypage();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentMain).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (menuItem.getItemId()){
+                case R.id.mainItem:
+                    transaction.replace(R.id.frameLayout,fragmentMain).commitAllowingStateLoss();
+                    break;
+                case R.id.recipeItem:
+                    transaction.replace(R.id.frameLayout,fragmentRecipe).commitAllowingStateLoss();
+                    break;
+                case R.id.mapItem:
+                    transaction.replace(R.id.frameLayout,fragmentMap).commitAllowingStateLoss();
+                    break;
+                case R.id.mypageItem:
+                    transaction.replace(R.id.frameLayout,fragmentMypage).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
+}
