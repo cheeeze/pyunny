abe1264ed637f3de6cb81e2da816fbd1be3c45c4
이채은 <dlcodms123@gmail.com>
2020-06-01 15:36:22 +0900

    [Frond-end] Axios 파일 추가
    - Productaxios.js
    - Mapaxios.js
    - Recipeaxios.js
    
    S02P31D102-104

---
+++ frontend/src/api/Recipeaxios.js
@@ -0,0 +1,128 @@
+import http from '../http-common'
+
+const getRecipePopularOrdered = (success, error) => {
+    http
+        .get('api/recipe_popular')
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const getRecipeRecentOrdered = (success, error) => {
+    http
+        .get('api/recipe_recent')
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const getRecipeBySearch = (data, success, error) => {
+    http
+        .get('api/recipe?keyword=' + data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const getRecipeById = (data, success, error) => {
+    http
+        .get('api/recipe/' + data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const insertRecipe = (data, success, error) => {
+    http
+        .post('/api/recipe', data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const updateRecipe = (data, success, error) => {
+    http
+        .put('/api/recipe', data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const deleteRecipe = (data, success, error) => {
+    http
+        .delete('/api/recipe/' + data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const getProductAutoComplete = (data, success, error) => {
+    http
+        .get('/api/autocomplete?keyword=' + data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const insertRecipeComment = (data, success, error) => {
+    http
+        .post('/api/recipe_comment', data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+const deleteRecipeComment = (data, success, error) => {
+    http
+        .delete('/api/recipe_comment/' + data)
+        .then((res) => {
+            success(res)
+        })
+        .catch((err) => {
+            error(err)
+        })
+}
+
+
+const axiosFunction = {
+    getRecipePopularOrdered: (data, success, error) => getRecipePopularOrdered(data, success, error),
+    getRecipeRecentOrdered: (data, success, error) => getRecipeRecentOrdered(data, success, error),
+    getRecipeBySearch: (data, success, error) => getRecipeBySearch(data, success, error),
+    getRecipeById: (data, success, error) => getRecipeById(data, success, error),
+    insertRecipe: (data, success, error) => insertRecipe(data, success, error),
+    updateRecipe: (data, success, error) => updateRecipe(data, success, error),
+    deleteRecipe: (data, success, error) => deleteRecipe(data, success, error),
+    getProductAutoComplete: (data, success, error) => getProductAutoComplete(data, success, error),
+    insertRecipeComment: (data, success, error) => insertRecipeComment(data, success, error),
+    deleteRecipeComment: (data, success, error) => deleteRecipeComment(data, success, error),
+    getComment: (data, success, error) => getComment(data, success, error),
+}
+
+export default axiosFunction
