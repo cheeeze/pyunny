5ac7e55bd33e555901409001437563cf639486e6
하승진 <up6760@gmail.com>
2020-06-04 10:25:56 +0900

    [Front-end] Fixed Errors
    에러 발생 부분 주석 처리 (미사용 부분)
    
    S02P22D105-116

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,61 +1,62 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import axios from '../api/axiosScript.js'
+import Vue from "vue";
+import Router from "vue-router";
+// import axios from '../api/axiosScript.js'
 // Routes
-import paths from './paths'
-
+import paths from "./paths";
 
 function route(path, view, name) {
   return {
     name: name || view,
     path,
     component: (resolve) => import(`@/views/${view}.vue`).then(resolve),
-  }
+  };
 }
 
-Vue.use(Router)
+Vue.use(Router);
 
 // Create a new router
 const router = new Router({
-  mode: 'history',
+  mode: "history",
   routes: paths
-      .map((path) => route(path.path, path.view, path.name))
-      .concat([{
-          path: '*',
-          redirect: '/',
-      }, ]),
+    .map((path) => route(path.path, path.view, path.name))
+    .concat([
+      {
+        path: "*",
+        redirect: "/",
+      },
+    ]),
   scrollBehavior(to, from, savedPosition) {
-      if (savedPosition) {
-          return savedPosition
-      }
-      if (to.hash) {
-          return {
-              selector: to.hash,
-          }
-      }
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
       return {
-          x: 0,
-          y: 0,
-      }
+        selector: to.hash,
+      };
+    }
+    return {
+      x: 0,
+      y: 0,
+    };
   },
-})
+});
 
-router.beforeEach((to, from, next) => {
-  let data = sessionStorage.getItem('session')
-  if (data != null) {
-      axios.sessionCheck(
-          data,
-          (res) => {
-              sessionStorage.setItem('session', JSON.stringify(res.data.session))
-              next()
-          },
-          (err) => {
-              sessionStorage.removeItem('session')
-              sessionx.state.login = false
-              next('/')
-          },
-      )
-  }
-  next()
-})
-export default router
+// router.beforeEach((to, from, next) => {
+//   let data = sessionStorage.getItem("session");
+//   if (data != null) {
+//     axios.sessionCheck(
+//       data,
+//       (res) => {
+//         sessionStorage.setItem("session", JSON.stringify(res.data.session));
+//         next();
+//       },
+//       (err) => {
+//         sessionStorage.removeItem("session");
+//         sessionx.state.login = false;
+//         next("/");
+//       }
+//     );
+//   }
+//   next();
+// });
+export default router;
