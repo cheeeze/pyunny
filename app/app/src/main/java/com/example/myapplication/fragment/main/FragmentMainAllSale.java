e1cb3bb23d00de7e6f832356e49722e1610243a8
신혜진 <hye19s@hanmail.net>
2020-06-09 09:09:16 +0900

    [App] Main 할인정보 상세정보
    Fragment 로 되어있던 할인정보 상세정보 탭을 버튼으로 변경
    버튼 클릭 시에 refresh되어 변경된 데이터가 프레그먼트에 반영됨
    
    S02P31D102-23

--- app/app/src/main/java/com/example/myapplication/fragment/main/FragmentMainAllSale.java
+++ app/app/src/main/java/com/example/myapplication/fragment/main/FragmentMainAllSale.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 import com.example.myapplication.adapter.MainSaleAdapter;
@@ -49,7 +51,34 @@ public class FragmentMainAllSale extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main_all_sale, container, false);
         init(view);
+        Button saleButton = (Button)view.findViewById(R.id.button_sale);
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mainSaleAdapter = new MainSaleAdapter(mContext,getList());
+                //recyclerView.setAdapter(mainSaleAdapter);
+                //mainSaleAdapter.notifyDataSetChanged();
+                getList();
+                refresh();
+            }
+        });
+        Button productButton = (Button)view.findViewById(R.id.button_all);
+        productButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getProductList();
+                //mainSaleAdapter = new MainSaleAdapter(mContext,getProductList());
+                //recyclerView.setAdapter(mainSaleAdapter);
+                //mainSaleAdapter.notifyDataSetChanged();
+                refresh();
+            }
+        });
         return view;
+    }
+
+    private void refresh() {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.detach(this).attach(this).commit();
     }
 
 
@@ -60,7 +89,7 @@ public class FragmentMainAllSale extends Fragment {
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(mContext,2);
         recyclerView.setLayoutManager(mLayoutManager);
         //getSaleList();
-        mainSaleAdapter = new MainSaleAdapter(mContext,getList());
+        mainSaleAdapter = new MainSaleAdapter(mContext,saleList);
         //mainSaleAdapter = new MainSaleAdapter(mContext,saleList);
 
 
@@ -142,20 +171,50 @@ public class FragmentMainAllSale extends Fragment {
 
 
     private ArrayList<Sale> getList(){
+        saleList.clear();
+        //int franchise_id, String type, String product_name, int product_price, String product_image
+        saleList.add(new Sale(682,"1+1","농심)멘토스레인보우",800,"http://bgf-cu.xcache.kinxcdn.com/product/6921211104292.jpg"));
+        saleList.add(new Sale(682,"1+1","롯)의성마늘프랑크2입",3300,"http://bgf-cu.xcache.kinxcdn.com/product/8801123308910.jpg"));
+        saleList.add(new Sale(682,"1+1","롯데)트로피애플캔355ml",1200,"http://bgf-cu.xcache.kinxcdn.com/product/8801056035570.jpg"));
+        saleList.add(new Sale(682,"1+1","동아)데미소다피치캔250ml",1200,"http://cdn2.bgfretail.com/bgfbrand/files/product/95C1BC12E8A74F62A04B3A4C1A3F9C0C.jpg"));
+        saleList.add(new Sale(682,"1+1","동아)데미소다애플캔250ml",1200,"http://cdn2.bgfretail.com/bgfbrand/files/product/B9054EE9A43448F58142E54DFA667DC0.jpg"));
+        saleList.add(new Sale(1,"2+1","롯데)크런키초콜릿",1000,"https://bgf-cu.xcache.kinxcdn.com/product/8801062628476.jpg"));
+        saleList.add(new Sale(3,"2+1","롯데)롱에센뽀득프랑크100g",2400,"https://emart24.co.kr/upload/eventProduct/FOOD/8801123310234.jpg"));
+        saleList.add(new Sale(646,"1+1","동원)상상바삭디핑칩고소한맛",3000,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801047184126_001.jpg"));
+        saleList.add(new Sale(646,"1+1","동원)상상바삭디핑칩매콤한맛",3000,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801047184133_001.jpg"));
+        saleList.add(new Sale(646,"1+1","해태)과일사이다딸기키위380ML",1500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801105916157_002.jpg"));
+        saleList.add(new Sale(646,"1+1","해태)과일사이다파인애플380ML",1500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801105916133_002.jpg"));
+        saleList.add(new Sale(646,"1+1","한끼불고기150G(달콤한맛)",4900,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8809152414794_001.jpg"));
+        saleList.add(new Sale(646,"1+1","한끼불고기150G(매콤한맛)",4900,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8809152414800_001.jpg"));
+
+
+        return saleList;
+    }
+
+    private ArrayList<Sale> getProductList(){
         ArrayList<Sale> list = new ArrayList<>();
         //int franchise_id, String type, String product_name, int product_price, String product_image
-        list.add(new Sale(0,"1+1","동아)데미소다레몬캔250ML",1200,"https://gs25appimg.gsretail.com/imgsvr/item/GD_8801097160071.jpg"));
-        list.add(new Sale(1,"2+1","롯데)크런키초콜릿",1000,"https://bgf-cu.xcache.kinxcdn.com/product/8801062628476.jpg"));
-        list.add(new Sale(3,"2+1","롯데)롱에센뽀득프랑크100g",2400,"https://emart24.co.kr/upload/eventProduct/FOOD/8801123310234.jpg"));
-        list.add(new Sale(0,"1+1","동아)데미소다레몬캔250ML",1200,"https://gs25appimg.gsretail.com/imgsvr/item/GD_8801097160071.jpg"));
-        list.add(new Sale(1,"2+1","롯데)크런키초콜릿",1000,"https://bgf-cu.xcache.kinxcdn.com/product/8801062628476.jpg"));
-        list.add(new Sale(3,"2+1","롯데)롱에센뽀득프랑크100g",2400,"https://emart24.co.kr/upload/eventProduct/FOOD/8801123310234.jpg"));
-        list.add(new Sale(0,"1+1","동아)데미소다레몬캔250ML",1200,"https://gs25appimg.gsretail.com/imgsvr/item/GD_8801097160071.jpg"));
-        list.add(new Sale(1,"2+1","롯데)크런키초콜릿",1000,"https://bgf-cu.xcache.kinxcdn.com/product/8801062628476.jpg"));
-        list.add(new Sale(3,"2+1","롯데)롱에센뽀득프랑크100g",2400,"https://emart24.co.kr/upload/eventProduct/FOOD/8801123310234.jpg"));
+        saleList.clear();
+        saleList.add(new Sale(682,null,"HEYROO콘치즈그라탕",3800,"http://bgf-cu.xcache.kinxcdn.com/product/8805684007732.jpg"));
+        saleList.add(new Sale(682,null,"튀김)핫도그",1000,"http://cdn2.bgfretail.com/bgfbrand/files/product/227743CDDAC242C59668F4E1087B7842.jpg"));
+        saleList.add(new Sale(682,null,"HEYROO뻥이요스낵1200",1200,"http://bgf-cu.xcache.kinxcdn.com/product/8801151112039.jpg"));
+        saleList.add(new Sale(682,null,"20델)베리베리에이드",700,"http://bgf-cu.xcache.kinxcdn.com/product/8809184806192.jpg"));
+        saleList.add(new Sale(682,null,"GET허쉬티라미수라떼30g",600,"http://cdn2.bgfretail.com/bgfbrand/files/product/7EA7380FC81B4ACF92F9007FA62B4D21.jpg"));
+        saleList.add(new Sale(646,null,"유어스)풀무원촉촉란100G",1900,"https://gs25appimg.gsretail.com/imgsvr/item/GD_8801097160071.jpg"));
+        saleList.add(new Sale(646,"2+1","유어스)펭럽유초코우유300ML",1500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801121027868_001.JPG"));
+        saleList.add(new Sale(646,"2+1","유어스)타이거슈가흑당밀크티300",2500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801155741280_001.jpg"));
+        saleList.add(new Sale(646,"1+1","유어스)젤리얌리치340ML[D2]",1500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8809329050411_002.jpg"));
+        saleList.add(new Sale(646,null,"케이준치킨텐더샌드위치1편",2500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_2700038824477_001.jpg"));
+        saleList.add(new Sale(646,null,"NEW고진많도시락1편",4500,"http://gs25appimg.gsretail.com/imgsvr/item/GD_2700038824354_001.jpg"));
+        saleList.add(new Sale(646,null,"왕비빔면1편",4000,"http://gs25appimg.gsretail.com/imgsvr/item/GD_2700038824101_001.jpg"));
+        saleList.add(new Sale(646,null,"군(軍)모닝버거1편",2800,"http://gs25appimg.gsretail.com/imgsvr/item/GD_2700038824378_001.JPG"));
+        saleList.add(new Sale(646,null,"유어스)돌덩이얼음3KG",3950,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8809197840275_001.jpg"));
+        saleList.add(new Sale(646,null,"유어스)돌덩이얼음1KG",1850,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8809197840268_001.jpg"));
+        saleList.add(new Sale(646,null,"유어스)소고기미역국500G",3900,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8809665555311_001.JPG"));
+        saleList.add(new Sale(646,null,"유어스)야쿠르트그랜드수박280ML",1300,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801128280709_001.JPG"));
+        saleList.add(new Sale(646,null,"유어스(P)얼그레이초코케이크",3000,"http://gs25appimg.gsretail.com/imgsvr/item/GD_8801062882090_001.jpg"));
 
-
-        return list;
+        return saleList;
     }
 
     private ArrayList<Sale> getRest(String value) {
