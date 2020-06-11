559e7253e192e3469431c316aa6f9d5a6d76eaf5
신혜진 <hye19s@hanmail.net>
2020-06-12 03:55:01 +0900

    [Back-end] Fix Swagger
    Swagger 페이지 요청 시 404 에러 문제 수정

--- backend/src/main/java/com/ssafy/backend/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/backend/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.ssafy.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport{
     
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -21,6 +23,14 @@ public class SwaggerConfig {
             .description("api swagger")
             .build();
     }
+
+    @Override
+     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+       // Make Swagger meta-data available via <baseURL>/v2/api-docs/
+       registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+       // Make Swagger UI available via <baseURL>/swagger-ui.html
+       registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
+     }
 
     @Bean
     public Docket api(){
